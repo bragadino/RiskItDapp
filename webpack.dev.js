08ff766e4628d71990c00a3a5d28d5e4794ea27a
Raymond Mogg <raymondmogg@gmail.com>
2018-04-27 21:01:22 +1000

    another mistake

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
-+ const merge = require('webpack-merge');
-+ const common = require('./webpack.common.js');
-+
-+ module.exports = merge(common, {
-+   devtool: 'inline-source-map',
-+   devServer: {
-+     contentBase: './dist'
-+   }
-+ });
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  }
+});
