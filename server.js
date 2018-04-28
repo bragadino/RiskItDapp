0c7c6e8e194753790399abd70d473e3a2f355e0f
Raymond Mogg <raymondmogg@gmail.com>
2018-04-28 21:18:23 +1000

    Trying with new server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static(_dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(_dirname, 'index.html'));
+});
+app.listen(port);
+console.log("Server started");
